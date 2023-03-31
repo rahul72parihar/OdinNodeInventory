213dded2f0c28a38cfa94443caf4e105c94f854d
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-03-31 10:35:06 +0530

    update readme

--- src/controller/index.js
+++ src/controller/index.js
@@ -6,6 +6,7 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+/* GET about page. */
 router.get("/about", function (req, res, next) {
   res.render("about", { title: "Express" });
 });
